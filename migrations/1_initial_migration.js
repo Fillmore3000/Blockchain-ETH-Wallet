36ae8f9d1e50e3a4531415c3a5a7e68b9b8a70ab
Henry Apaw <45966616+Fillmore3000@users.noreply.github.com>
2021-03-21 13:18:00 +0000

    Add files via upload

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0x5D09063b0e48EE4b9614406546D27C42da0619bc',
+    '1000000000000000000000'
+  )
+};
